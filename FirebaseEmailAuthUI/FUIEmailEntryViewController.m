cd110a62214147048834d45432d12a94aa9d3d98
Teewoon Tan <ttan@tanadium.com>
2018-12-17 16:50:04 +0800

    Temporary workaround for email authentication bug
    When an account is linked to multiple providers that includes an email provider, the user can no longer log in using email authentication. The bug seems to lie within the method, 'bestProviderFromProviderIDs', that finds the best provider to use for log in. The workaround performs a 'signIn' operation using the email provided by the user and a password that is guaranteed to fail, and then examining the return error. We want to know whether the email corresponds to a wrong password (meaning the user exists), or 'user not found'.

--- FirebaseEmailAuthUI/FUIEmailEntryViewController.m
+++ FirebaseEmailAuthUI/FUIEmailEntryViewController.m
@@ -29,6 +29,11 @@
 #import "FUIPasswordSignUpViewController.h"
 #import "FUIPrivacyAndTermsOfServiceView.h"
 
+/** @var MaxEmailPasswordSuccessRetry
+    @brief The maximum number of times an email-password sign-in is allowed to succeed.
+ */
+static unsigned int const MaxEmailPasswordSuccessRetry = 2;
+
 /** @var kCellReuseIdentifier
     @brief The reuse identifier for table view cell.
  */
@@ -161,49 +166,52 @@ static NSString *const kNextButtonAccessibilityID = @"NextButtonAccessibilityID"
       return;
     }
 
-    id<FUIAuthProvider> provider = [self bestProviderFromProviderIDs:providers];
-    if (provider && ![provider.providerID isEqualToString:FIREmailAuthProviderID]) {
-      NSString *email = emailText;
-      [[self class] showSignInAlertWithEmail:email
-                                    provider:provider
-                    presentingViewController:self
-                               signinHandler:^{
-        [self signInWithProvider:provider email:email];
-      }
-                               cancelHandler:^{
-        [self.authUI signOutWithError:nil];
-      }];
-    } else if ([providers containsObject:FIREmailAuthProviderID]) {
-      UIViewController *controller;
-      if ([delegate respondsToSelector:@selector(passwordSignInViewControllerForAuthUI:email:)]) {
-        controller = [delegate passwordSignInViewControllerForAuthUI:self.authUI
-                                                               email:emailText];
-      } else {
-        controller = [[FUIPasswordSignInViewController alloc] initWithAuthUI:self.authUI
-                                                                       email:emailText];
-      }
-      [self pushViewController:controller];
-    } else {
-      if (providers.count) {
-        // There's some unsupported providers, surface the error to the user.
-        [self showAlertWithMessage:FUILocalizedString(kStr_CannotAuthenticateError)];
-      } else {
-        // New user.
-        UIViewController *controller;
-        if (emailAuth.allowNewEmailAccounts) {
-          if ([delegate respondsToSelector:@selector(passwordSignUpViewControllerForAuthUI:email:)]) {
-            controller = [delegate passwordSignUpViewControllerForAuthUI:self.authUI
+    [self bestProviderFromProviderIDs:providers
+                            emailText:emailText
+                           completion:^(id<FUIAuthProvider> _Nullable provider) {
+        if (provider && ![provider.providerID isEqualToString:FIREmailAuthProviderID]) {
+          NSString *email = emailText;
+          [[self class] showSignInAlertWithEmail:email
+                                        provider:provider
+                        presentingViewController:self
+                                   signinHandler:^{
+            [self signInWithProvider:provider email:email];
+          }
+                                   cancelHandler:^{
+            [self.authUI signOutWithError:nil];
+          }];
+        } else if ([providers containsObject:FIREmailAuthProviderID]) {
+          UIViewController *controller;
+          if ([delegate respondsToSelector:@selector(passwordSignInViewControllerForAuthUI:email:)]) {
+            controller = [delegate passwordSignInViewControllerForAuthUI:self.authUI
                                                                    email:emailText];
           } else {
-            controller = [[FUIPasswordSignUpViewController alloc] initWithAuthUI:self.authUI
-                                                                         email:emailText];
+            controller = [[FUIPasswordSignInViewController alloc] initWithAuthUI:self.authUI
+                                                                           email:emailText];
           }
+          [self pushViewController:controller];
         } else {
-            [self showAlertWithMessage:FUILocalizedString(kStr_UserNotFoundError)];
+          if (providers.count) {
+            // There's some unsupported providers, surface the error to the user.
+            [self showAlertWithMessage:FUILocalizedString(kStr_CannotAuthenticateError)];
+          } else {
+            // New user.
+            UIViewController *controller;
+            if (emailAuth.allowNewEmailAccounts) {
+              if ([delegate respondsToSelector:@selector(passwordSignUpViewControllerForAuthUI:email:)]) {
+                controller = [delegate passwordSignUpViewControllerForAuthUI:self.authUI
+                                                                       email:emailText];
+              } else {
+                controller = [[FUIPasswordSignUpViewController alloc] initWithAuthUI:self.authUI
+                                                                             email:emailText];
+              }
+            } else {
+                [self showAlertWithMessage:FUILocalizedString(kStr_UserNotFoundError)];
+            }
+            [self pushViewController:controller];
+          }
         }
-        [self pushViewController:controller];
-      }
-    }
+    }];
   }];
 }
 
@@ -248,18 +256,6 @@ static NSString *const kNextButtonAccessibilityID = @"NextButtonAccessibilityID"
            forControlEvents:UIControlEventEditingChanged];
   [self didChangeEmail:_emailField.text];
   return cell;
-}
-
-- (nullable id<FUIAuthProvider>)bestProviderFromProviderIDs:(NSArray<NSString *> *)providerIDs {
-  NSArray<id<FUIAuthProvider>> *providers = self.authUI.providers;
-  for (NSString *providerID in providerIDs) {
-    for (id<FUIAuthProvider> provider in providers) {
-      if ([providerID isEqual:provider.providerID]) {
-        return provider;
-      }
-    }
-  }
-  return nil;
 }
 
 #pragma mark - UITextFieldDelegate
@@ -318,4 +314,146 @@ static NSString *const kNextButtonAccessibilityID = @"NextButtonAccessibilityID"
     }];
  }];
 }
+
+typedef void (^BestProviderQueryCallback)(id<FUIAuthProvider> _Nullable bestProvider);
+
+/** @fn bestProviderFromProviderIDs:emailText:completion:
+ @brief Looks for the best provider given the array of provider ids that the user can use to sign in with.
+ @param providerIDs Array of provider ids that the user can use to sign in with.
+ @param emailText The email address of the user.
+ @param completion The block to be executed on completion once the best provider is found. If none is found then
+                   nil is passed to the completion block.
+ */
+- (void)bestProviderFromProviderIDs:(NSArray<NSString *> *)providerIDs
+                          emailText:(NSString *)emailText
+                         completion:(BestProviderQueryCallback)completion {
+  if ([providerIDs containsObject:FIREmailAuthProviderID]) {
+    [self handleEmailOrOtherLoginWithProviderIDs:providerIDs
+                                       emailText:emailText
+                                      completion:completion
+                                     timesCalled:0];
+  }
+  else {
+    [self anyProviderFromProviderIDs:providerIDs
+                         exceptEmail:NO
+                          completion:completion];
+  }
+}
+
+/** @fn anyProviderFromProviderIDs:exceptEmail:completion:
+ @brief Looks for any provider from the given the array of provider ids that the user can use to sign in with.
+ @param providerIDs Array of provider ids that the user can use to sign in with.
+ @param exceptEmail Set this flag to true if email provider should be ignored.
+ @param completion The block to be executed on completion once the best provider is found. If none is found then
+                   nil is passed to the completion block.
+ */
+- (void)anyProviderFromProviderIDs:(NSArray<NSString *> *)providerIDs
+                       exceptEmail:(BOOL)isExceptEmail
+                        completion:(BestProviderQueryCallback)completion {
+  id<FUIAuthProvider> bestProvider = nil;
+  NSArray<id<FUIAuthProvider>> *providers = self.authUI.providers;
+  for (NSString *providerID in providerIDs) {
+    if (isExceptEmail &&
+        [providerID isEqualToString:FIREmailAuthProviderID]) {
+      // Ignore email sign-in method
+      continue;
+    }
+    for (id<FUIAuthProvider> provider in providers) {
+      if ([providerID isEqual:provider.providerID]) {
+        bestProvider = provider;
+        break;
+      }
+    }
+  }
+  completion(bestProvider);
+}
+
+/** @fn handleEmailOrOtherLoginWithProviderIDs:emailText:completion:
+ @brief Given the array of provider ids that the user can use to sign in with, find the best login method whilst
+        taking into account that the user-provided email address may or may not point to an account that has
+        an email authentication method.
+ @param providerIDs Array of provider ids that the user can use to sign in with.
+ @param emailText The email address of the user.
+ @param completion The block to be executed on completion once the best provider is found. If none is found then
+                   nil is passed to the completion block.
+ @param timesCalled The number of times this method has been called. This method may recurse, and this variable
+                    is incremented each time it is called.
+ */
+- (void)handleEmailOrOtherLoginWithProviderIDs:(NSArray<NSString *> *)providerIDs
+                                     emailText:(NSString *)emailText
+                                    completion:(BestProviderQueryCallback)completion
+                                   timesCalled:(unsigned int)timesCalled {
+  if (timesCalled >= MaxEmailPasswordSuccessRetry) {
+    completion(nil);
+    return; // Something wrong - we keep guessing the correct password!!!
+  }
+  NSString *randomPassword = [self getRandomPassword];
+  [self.auth signInWithEmail:emailText
+                    password:randomPassword
+                  completion:^(FIRAuthDataResult *_Nullable authDataResult,
+                               NSError *_Nullable error) {
+    if (error) {
+      // An error must occur because we use a random, pretty much guaranteed-to-fail, password.
+      // We do this just to find out if a user with the given email exists on the system.
+      // A disadvantage of this method is that the user is allowed one less password attempt
+      // because we already made one false attempt. If the user keeps trying
+      // to log in using an email address and keep cancelling at the password stage then
+      // they will rack up incorrect log-in attempts. What happens after that depends on
+      // password and security settings. The user may need to wait a few minutes (seconds?) before
+      // logging in again.
+      if (error.code == FIRAuthErrorCodeWrongPassword) {
+        // Wrong password, but email is valid, so handle log in using the email provider
+        [self handleEmailProviderCompletion:completion];
+      }
+      else if (error.code == FIRAuthErrorCodeUserNotFound) {
+        // User email does not exist, so try to log in using a provider other than email
+        [self anyProviderFromProviderIDs:providerIDs
+                             exceptEmail:YES // Ignore email sign-in because a user with the given email is not found
+                              completion:completion];
+      }
+      else {
+        // Some other error occurred
+        completion(nil);
+      }
+      return;
+    }
+    // The password was accepted!!! Try again, after signing out, with another random password.
+    // Although we can proceed after we had success with a fluked random password, we want to
+    // try again with another random password to make sure something funny isn't happening.
+    [self.auth signOut:nil]; // Sign out immediately if we happen to get the right password - do not store anywhere
+    [self handleEmailOrOtherLoginWithProviderIDs:providerIDs
+                                       emailText:emailText
+                                      completion:completion
+                                     timesCalled:timesCalled + 1];
+  }];
+}
+
+/** @fn handleEmailProviderCompletion:completion:
+ @brief Calls the completion block with the email authentication provider.
+ @param completion The block to be executed on completion once the best provider is found. If none is found then
+                   nil is passed to the completion block.
+ */
+- (void)handleEmailProviderCompletion:(BestProviderQueryCallback)completion {
+  NSArray<id<FUIAuthProvider>> *providers = self.authUI.providers;
+  NSPredicate *emailPred = [NSPredicate predicateWithFormat:@"SELF.providerID IN %@", FIREmailAuthProviderID];
+  id<FUIAuthProvider> emailProvider = [[providers filteredArrayUsingPredicate:emailPred] firstObject];
+  assert(emailProvider != nil);
+  completion(emailProvider);
+}
+
+/** @fn getRandomPassword
+ @brief Generates a random password that is based on UUIDs. UUIDs are used for uniqueness only, and does not
+        in anyway imply password strength, which is not our objective here. This method should not be used to
+        generate passwords that are actually used to secure an account. This method is used to generate a password
+        that ensures log-in failure when signing in to any account.
+ */
+- (NSString *)getRandomPassword {
+  NSUUID *uuid1 = [NSUUID UUID];
+  NSUUID *uuid2 = [NSUUID UUID];
+  // Give it some extra randomness to make sure we don't guess someone's password
+  NSString *randomPassword = [[uuid1 UUIDString] stringByAppendingString:[uuid2 UUIDString]];
+  
+  return randomPassword;
+}
+
 @end
